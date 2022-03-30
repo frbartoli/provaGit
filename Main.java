5cbeebdc310f0b567b001dd5bdd4d077ae55519c
Francesco Bartoli <francesco.bartoli@edu.unife.it>
2022-03-30 17:26:39 +0200

    Commit extra

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ package provaGit;
 public class Main {
     public static void main(String args[]){
         System.out.println("Hello world modificato!");
+        System.out.println("Hello world bis");
     }
 }
