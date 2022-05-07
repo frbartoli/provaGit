300e4800be1bb9462e2c16a574ba2272218a5f9d
frbartoli <88556976+frbartoli@users.noreply.github.com>
2022-05-07 19:30:18 +0200

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-package provaGit;
-
-public class Main {
-    public static void main(String args[]){
-        System.out.println("Hello world modificato!");
-        System.out.println("Hello world bis");
-    }
-}
